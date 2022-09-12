2303dcc17d7a7e90fd26346c2a22007ac36f8d23
vascocunha21 <vascoc99@ua.pt>
2022-09-12 16:13:32 +0100

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import NotificationButton from "./components/NotificationButton"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
-
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
@@ -14,8 +15,6 @@ function App() {
           </div>
         </section>
       </main>
-      <NotificationButton />
-      <NotificationButton />
     </>
   )
 }
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 
@@ -8,14 +9,14 @@ type Props = {
     saleId: number;
 }
 
-function handleClick(id: number){
+function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO");
+            toast.info("SMS enviado com sucesso")
         })
 }
 
-function NotificationButton({saleId}: Props) {
+function NotificationButton({ saleId }: Props) {
     return (
         <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
